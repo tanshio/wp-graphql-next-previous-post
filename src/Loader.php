6a910dfcd167bcc3d42d999f20f3ea280697e808
Shota Tanno <rakuburomail+tanshio@gmail.com>
2022-05-04 06:55:01 +0900

    Fix and add custom post type

--- src/Loader.php
+++ src/Loader.php
@@ -16,76 +16,90 @@ use WPGraphQL\Model\Post;
  */
 class Loader
 {
-    public static function init()
-    {
-        define('WP_GRAPHQL_NEXT_PREVIOUS_POST', 'initialized');
-        (new Loader())->bind_hooks();
-    }
+	public static function init()
+	{
+		define('WP_GRAPHQL_NEXT_PREVIOUS_POST', 'initialized');
+		(new Loader())->bind_hooks();
+	}
 
-    public function bind_hooks()
-    {
-        add_action(
-            'graphql_register_types',
-            [$this, 'npp_action_register_types'],
-            9,
-            0
-        );
+	public function bind_hooks()
+	{
+		add_action(
+			'graphql_register_types',
+			[$this, 'npp_action_register_types'],
+			9,
+			0
+		);
 
-    }
+	}
 
-    public function npp_action_register_types()
-    {
-        register_graphql_field('Post', 'next', [
-            'type' => 'Post',
-            'description' => __(
-                'Next post'
-            ),
-            'resolve' => function (Post $post, array $args, AppContext $context) {
-                global $post;
+	public function npp_action_register_types()
+	{
+		$args = [
+			'show_in_nav_menus' => true,
+		];
+		$post_types = \WPGraphQL::get_allowed_post_types($args);
+		if (!empty($post_types) && is_array($post_types)) {
 
-                // get post
-                $post = get_post($post->ID, OBJECT);
+			foreach ($post_types as $post_type) {
+				$post_type_object = get_post_type_object($post_type);
+				if (isset($post_type_object->graphql_single_name)) {
+					$name = $post_type_object->graphql_single_name;
+					register_graphql_field($name, 'next', [
+						'type' => 'Post',
+						'description' => __(
+							'Next post'
+						),
+						'resolve' => function (Post $_post, array $args, AppContext $context) {
+							global $post;
 
-                // setup global $post variable
-                setup_postdata($post);
+							// get post
+							$post = get_post($_post->ID, OBJECT);
 
-                $next = get_next_post();
+							// setup global $post variable
+							setup_postdata($post);
 
-                wp_reset_postdata();
+							$next = get_next_post();
+							$post_id = $post->ID;
 
-                if (!$next) {
-                    return null;
-                }
+							wp_reset_postdata();
 
-                return DataSource::resolve_post_object($next->ID, $context);
-            },
-        ]);
+							if (!$next) {
+								return null;
+							}
 
-        register_graphql_field('Post', 'previous', [
-            'type' => 'Post',
-            'description' => __(
-                'Previous post'
-            ),
+							return DataSource::resolve_post_object($next->ID, $context);
+						},
+					]);
 
-            'resolve' => function (Post $post, array $args, AppContext $context) {
-                global $post;
+					register_graphql_field($name, 'previous', [
+						'type' => 'Post',
+						'description' => __(
+							'Previous post'
+						),
 
-                // get post
-                $post = get_post($post->ID, OBJECT);
+						'resolve' => function (Post $_post, array $args, AppContext $context) {
+							global $post;
 
-                // setup global $post variable
-                setup_postdata($post);
+							// get post
+							$post = get_post($_post->ID, OBJECT);
 
-                $prev = get_previous_post();
+							// setup global $post variable
+							setup_postdata($post);
 
-                wp_reset_postdata();
+							$prev = get_previous_post();
 
-                if (!$prev) {
-                    return null;
-                }
+							wp_reset_postdata();
 
-                return DataSource::resolve_post_object($prev->ID, $context);
-            },
-        ]);
-    }
+							if (!$prev) {
+								return null;
+							}
+
+							return DataSource::resolve_post_object($prev->ID, $context);
+						},
+					]);
+				}
+			}
+		}
+	}
 }
